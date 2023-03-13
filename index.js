d4df5e0dce84bd914c2654e62d89f19f67109514
Ganesh <ganeshnpawar1987@gmail.com>
2023-03-13 22:41:02 +0530

    variable section completed

--- index.js
+++ index.js
@@ -2,4 +2,44 @@ console.log("Hello World");
 console.log("Hello ganesh");
 
 //  Section 3 Values and variables
-var fName = "Ganesh";
+
+
+// console.log("Hello World");
+// console.log("Hello ganesh");
+
+//***** */  Section 3 Values and variables
+
+// var fName= "Ganesh";      
+/*var is a keyword, fName is a variable name, "Ganesh" is a Value.*/
+// console.log(fName);
+
+// ****** Challenge Time:
+
+// var _myName = "Ganesh";      //true
+// var 1myName = "Pawar";        //false
+// var _1my__Name = "Laxmi";     //true 
+// var $myName = "Vaishali";      //true
+// var $myName% = "Ganesh Pawar";    //false
+
+// console.log($myName%);
+
+
+// console.log("Hello World");
+// console.log("Hello ganesh");
+
+//***** */  Section 3 Values and variables
+
+// var fName= "Ganesh";      
+/*var is a keyword, fName is a variable name, "Ganesh" is a Value.*/
+// console.log(fName);
+
+// ****** Challenge Time:
+
+// var _myName = "Ganesh";      //true
+// var 1myName = "Pawar";        //false
+// var _1my__Name = "Laxmi";     //true 
+// var $myName = "Vaishali";      //true
+// var $myName% = "Ganesh Pawar";    //false
+
+// console.log($myName%);
+

048b573f260dde800d851acdd7a828e5803e3a79
Ganesh <ganeshnpawar1987@gmail.com>
2023-03-14 00:21:05 +0530

    operator section completed

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
-console.log("Hello World");
-console.log("Hello ganesh");
+//****   Section 5 Expression and Operators
 
-//  Section 3 Values and variables
-var fName = "Ganesh";
+// 1) Assignment operator  =
+// 2) Arithmetic operator
+// 3) Comparation operator
+// 4) Logical operator
+// 5) String operator
+// 6) Conditional (tenary) operator 
+
+
+// Challenge time:
+
+// 1) What will be the output of 3**3?
+
+// console.log(3**3);  // 27
+
+// 2)What will be output ,when we add number and string?
+// console.log(10 + "Ganesh");  // 10Ganesh
+
+// 3) Write a program to swap two numbers?
+
+// var a = 5;
+// var b = 10;
+
+// var c = b;
+//  b = a;
+//  a = c;
+
+// console.log(a);  // 10
+// console.log(b);  // 5
+
+// 4) Write a program to swaptwo  numbers without using third variable?
+
+// var a = 5;
+// var b = 10;
+
+// b = b-a;   // 10-5=5
+// a = b+a;   // 5+5 =10
+
+// OR
+// a = a + b;  // 5+10= 15
+// b = a-b;     // 15-10=5
+// a = a-b;   // 10
+
+// console.log(a);  // 10
+// console.log(b);  // 5
+
+
+// * Interview Question
+
+// 1) What is the difference between == vs === ?
+
+// var a = 5;
+// var b = '5';
+
+// console.log(a == b); // true   it compares only value not data types.
+// console.log(a === b);  // false it comparers both the value as well as data types also.

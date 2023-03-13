bd45dc5429bae59317d75da64d7b4c52678dceee
Ganesh <ganeshnpawar1987@gmail.com>
2023-03-14 02:21:10 +0530

    loop section completed

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 // Control Statement and Loops
 
-if... else 
-switch statement 
-while loop 
-do-while loop
-for loop 
-for in loop 
-for of loop 
-conditional (ternary) operator
+// if... else 
+// switch statement 
+// while loop 
+// do-while loop
+// for loop 
+// for in loop 
+// for of loop 
+// conditional (ternary) operator
+
+
+// for (var num=0; num<=10; num++){
+//     debugger;
+//     console.log(num);
+// }
+
+
+// Challenge Time 
+
+// javascript program to print table  for given number?
+
+// for(var num=1; num<=10; num++){
+//     console.log(num + "=" + num * 8 );
+
+// }

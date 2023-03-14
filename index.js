360f6785dfd3775cbd16cf6299535524c8fc3aa6
Ganesh <ganeshnpawar1987@gmail.com>
2023-03-14 13:22:14 +0530

    function section is completed

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-console.log("Hello World");
-console.log("Hello ganesh");
+// What is function?
 
-//  Section 3 Values and variables
-var fName = "Ganesh";
+// Ans=> function is a block of code which is used to perform perticular task.
+// we can reuse that code in future multiple times.
+
+// Function syntax:: function functionName(parameters){
+// statments
+// }
+
+// Interview Question
+
+// 1) What is the difference between function parameters and function arguments?
+// Ans=> function parameters  are the names listed in the function definition.
+// function arguments are the real values passed to the function.
+
+// ** Function Expressions?
+// ans=> Function expression simply means create a function and put it into the variable.
+
+// Ex::
+// function sum(a,b){
+//     return total = a+b;
+// }
+
+// var funExp = sum(10,20);
+// console.log('The sum of two no is:' + funExp);
+
+// Anonymous Function=>
+
+// Anonymous function mean which fucntion does not contain the Name.Anonymous
+
+// Ex:
+//   var funExp = function(a, b) {
+//     return total = a + b;
+//   };
+
+// var sum = funExp(30, 20);
+// console.log("The sum of two no is:" + sum);

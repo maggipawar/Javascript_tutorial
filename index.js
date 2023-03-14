4368df5bb0400d61efe81f6701f54c3ffed11768
Ganesh <ganeshnpawar1987@gmail.com>
2023-03-14 17:36:55 +0530

    Half ECMAScript_section completed

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-console.log("Hello World");
-console.log("Hello ganesh");
+// *** ECMAScript *** //
 
-//  Section 3 Values and variables
-var fName = "Ganesh";
+// let,Const and let 
+
+// var myFisrtName = "Ganesh";
+// console.log(myFisrtName);
+// var myFisrtName = "Pawar";
+// console.log(myFisrtName);
+
+
+// let myFisrtName = "Ganesh";
+// console.log(myFisrtName);
+// let myFisrtName = "Pawar";
+// console.log(myFisrtName);
+
+
+// const myFisrtName = "Ganesh";
+// console.log(myFisrtName);
+//  myFisrtName = "Pawar";
+// console.log(myFisrtName);
+
+
+// ***  Template Literal
+
+// for(var num=1; num<=10; num++){
+//     var tableOf = 12;
+//     console.log(`${tableOf} * ${num} = ${tableOf * num}`);
+// }
+
+
+// ** Fat arrow function ** //
+
+// const sum = (a,b) => `The sum of two number is ${a+b}`;
+
+// console.log(sum(5,5));

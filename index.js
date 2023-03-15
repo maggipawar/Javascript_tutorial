b969fc9f2d37e9b423d942610fb4e8e647f95d6d
Ganesh <ganeshnpawar1987@gmail.com>
2023-03-15 19:04:24 +0530

    date time and Math object methods completed

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-console.log("Hello World");
-console.log("Hello ganesh");
+// ** Date and Time i javascript   ** //
 
-//  Section 3 Values and variables
-var fName = "Ganesh";
+// That represents milliseconds since 1 january 1970 UTC.
+
+// creating Date Objects
+
+// There are 4 ways to create Date objects
+// new Date()
+// new Date(year,month,day,hours,minute,second,millisecond);
+// new Date(millisecond);
+// new Date(date string);
+
+// javascript counts month from 0 to 11
+// 11 = December month,  0 = January month 
+
+
+// ** Math Objects in Javascript ** //
+
+// 1) Math.round()
+// 2) Math.pow()
+// 3) Math.sqrt()
+// 4) Math.abs(x) absolute  returns the only positive value of x.
+// 5) Math.ceil(x) returns the value of x rounded up to its nearest integer. 
+// 6) Math.floor(x)  returns the value of x rounded Down to its nearest integer.
+// 7) Math.min(x)
+// 8) Math.max(x)
+// 9) Math.random() it return random value.
+// 10) Math.trunc(x) it return only integer value always.

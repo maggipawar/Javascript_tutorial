60d00ebd902f86d4584ab24d84a2fa8a1ab0b8f7
Ganesh <ganeshnpawar1987@gmail.com>
2023-03-16 16:33:00 +0530

    DOM with practice part

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 // ** DOM  Section ** //
 
+// ** Events in Javascript ** //
+
+// 4 ways of writting Events in javscript Events
+
+// 1) What is event object?
+
+// 2) MouseEvent in javascript
+
+// 3) KeyboardEvent in javascript
+
+// 4) InputEvent in javascript
+
+
+
+
+// 2) MouseEvent in javascript
+
+// i) mouseUp()
+// ii) mouseDown()
+// iii)mouseenter()
+// iv) mouseleave()
+
+
+
